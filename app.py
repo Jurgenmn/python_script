ebb9a91705ab517805152e3190c5cf9282b68d63
Jurgenmn <jurgenmane@gmail.com>
2022-08-31 20:16:08 -0500

    New feature: Add employees to the csv file

--- app.py
+++ app.py
@@ -34,8 +34,22 @@ def search_by_date(file, date):
     return matches
 
 
+# def write_employee_to_csv(file_name, name, surname, dpt, sdate):
+#     file = open(file_name, "a")  #append
+#     csv_row = f"{name},{surname},{dpt},{sdate}\n"
+#     file.write(csv_row)
+
+def write_employee_to_csv(file_name, name, surname, dpt, sdate):
+    file = open(file_name, "a")
+    speamwriter = csv.writer(file, delimiter=",")
+    #columns = [name, surname, dpt, sdate]
+    speamwriter.writerow([name, surname, dpt, sdate]) #takes a list
+
+
+
+
 def main():
-    option = input("1- Search by department\n2- Search by date\n: ")
+    option = input("1- Search by department\n2- Search by date\n3- Add new employ\n: ")
     if option == "1":
         department = input("Department name: ")
         result = search_by_dep("employees.csv", department)
@@ -46,6 +60,14 @@ def main():
         result = search_by_date("employees.csv", date)
         for emp in result:
             print(emp)
+
+    if option == "3":
+        employee_name = input("Name :")
+        employee_surname = input("Surname :")
+        employee_dpt = input("Department :")
+        employee_start_date = input("Start date ex:2020-04-26 :")
+        write_employee_to_csv("employees.csv", employee_name, employee_surname, employee_dpt, employee_start_date)
+
 
 
 main()
